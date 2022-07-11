8e62aecfec630ac192ddf1e9cbd85b0aac366afe
DavidNaili <95352262+DavidNaili@users.noreply.github.com>
2022-07-11 13:40:57 +0200

    H2 Datenbank implementiert

--- src/main/java/com/example/WebService/WebServiceApplication.java
+++ src/main/java/com/example/WebService/WebServiceApplication.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -92,8 +95,18 @@ public class WebServiceApplication {
     return ret;
 }
 
+static String jdbcURL = "jdbc:h2:~/WebService";
+static String username = "Naili";
+static String password = "nami1980";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(WebServiceApplication.class, args);
+
+	Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+    	System.out.println("Connected to H2 embedded database.");
+
+    connection.close();
+		}
+
 	}
-}
+	
