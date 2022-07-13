ed0d0847cdc621500abb9afb5e692e8e2a2f2b56
DavidNaili <95352262+DavidNaili@users.noreply.github.com>
2022-07-13 17:48:25 +0200

    Änderungen 13.07.2022

---
+++ src/main/java/com/example/WebService/MimeTypes.java
@@ -0,0 +1,23 @@
+package com.example.WebService;
+
+public class MimeTypes {
+
+    public static final String IMAGE_PNG = "image/png";
+    public static final String IMAGE_APNG = "image/apng";
+    public static final String IMAGE_JPG = "image/jpg";
+    public static final String IMAGE_JPEG = "image/jpeg";
+    public static final String IMAGE_PDF = "image/pdf";
+    public static final String IMAGE_GIF = "image/gif";
+    public static final String XLS = "application/vnd.ms-excel";
+    public static final String APPLICATION_ZIP = "application/zip";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String CSV = "text/csv";
+    public static final String CSS = "text/css";
+    public static final String HTML = "text/html";
+    public static final String PHP = "text/php";
+    public static final String PLAIN = "text/plain";
+    public static final String XML = "text/xml";
+    public static final String AUDIO_MPEG = "audio/mpeg";
+    public static final String AUDIO_OGG = "audio/ogg";
+    
+}
--- src/main/java/com/example/WebService/WebServiceApplication.java
+++ src/main/java/com/example/WebService/WebServiceApplication.java
@@ -1,46 +1,58 @@
 package com.example.WebService;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicException;
+import net.sf.jmimemagic.MagicMatchNotFoundException;
+import net.sf.jmimemagic.MagicParseException;
+
 @SpringBootApplication
 @Controller
-public class WebServiceApplication {
-
-	@GetMapping("/success")
-	public String success(){
-		return "success";
-	}
-	@GetMapping("/error")
-	public String error(){
-		return "error";
-	}
-
-	@PostMapping("/upload")
-	public String uploadFile(@RequestParam("file")MultipartFile file)
-		throws IllegalStateException, IOException {
-
-		String baseDir = "F:/Projekt/WebService/src/main/resources/static/upload/";
+public class WebServiceApplication{
 	
-	if (file.getOriginalFilename().endsWith(".csv")){
-		file.transferTo(new File(baseDir + file.getOriginalFilename()));
-		return "redirect:/success";
-	}
-	else
-	{return "redirect:/error";}
+	@RequestMapping (value = "/", method = RequestMethod.GET)
+    public String get(HttpServletRequest request) {
+        return "fileUpload";
 	}
 
+	@RequestMapping (value = "/uploadFile", method=RequestMethod.POST)
+	public String saveFile(@RequestParam("file") MultipartFile file, Model model)
+		throws MagicParseException, MagicMatchNotFoundException, MagicException, IOException {
+			System.out.println("--------" + file.getOriginalFilename());
+
+			String baseDir = "F:/Projekt/WebService/src/main/resources/static/upload/";
+	
+	if (!file.getOriginalFilename().matches("^.*(png)$")){
+			model.addAttribute("Error", Boolean.TRUE);
+			return "fileUpload";
+	}
+	String mimeType = Magic.getMagicMatch(file.getBytes()).getMimeType();
+	if (!mimeType.equalsIgnoreCase(MimeTypes.CSV)){
+		model.addAttribute("mimeError", Boolean.TRUE);
+		return "fileUpload";
+	}
+	model.addAttribute("success", Boolean.TRUE);
+	file.transferTo(new File(baseDir + file.getOriginalFilename()));
+	return "fileUpload";
+}
+	
 static String jdbcURL = "jdbc:h2:~/WebService";
 static String username = "Naili";
 static String password = "nami1980";
