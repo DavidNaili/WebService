77bfe060fce17b24c9840061976678161b1cd5d9
DavidNaili <95352262+DavidNaili@users.noreply.github.com>
2022-08-09 11:55:57 +0200

    Änderungen 09.08.2022

--- src/main/java/com/example/WebService/WebServiceApplication.java
+++ src/main/java/com/example/WebService/WebServiceApplication.java
@@ -1,66 +1,17 @@
 package com.example.WebService;
 
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import net.sf.jmimemagic.Magic;
-import net.sf.jmimemagic.MagicException;
-import net.sf.jmimemagic.MagicMatchNotFoundException;
-import net.sf.jmimemagic.MagicParseException;
 
 @SpringBootApplication
-@Controller
+
 public class WebServiceApplication{
 	
-	@RequestMapping (value = "/", method = RequestMethod.GET)
-    public String get(HttpServletRequest request) {
-        return "fileUpload";
-	}
-
-	/**
-	 * @param file
-	 * @param model
-	 * @return
-	 * @throws MagicParseException
-	 * @throws MagicMatchNotFoundException
-	 * @throws MagicException
-	 * @throws IOException
-	 */
-	@RequestMapping (value = "/uploadFile", method=RequestMethod.POST)
-	public String saveFile(@RequestParam("file") MultipartFile file, Model model)
-		throws MagicParseException, MagicMatchNotFoundException, MagicException, IOException {
-			System.out.println("--------" + file.getOriginalFilename());
-
-			String baseDir = "F:/Projekt/WebService/src/main/resources/static/upload/";
-	
-	if (!file.getOriginalFilename().matches("^.*(jpg, png)$")){
-			model.addAttribute("Error", Boolean.TRUE);
-			return "fileUpload";
-	}
-	String mimeType = Magic.getMagicMatch(file.getBytes()).getMimeType();
-	if (!mimeType.equalsIgnoreCase(MimeTypes.CSV)){
-		model.addAttribute("mimeError", Boolean.TRUE);
-		return "fileUpload";
-	}
-	model.addAttribute("success", Boolean.TRUE);
-	file.transferTo(new File(baseDir + file.getOriginalFilename()));
-	return "fileUpload";
-}
 	
 static String jdbcURL = "jdbc:h2:~/WebService";
 static String username = "Naili";
---
+++ src/main/java/com/example/WebService/controllers/AppController.java
@@ -0,0 +1,59 @@
+package com.example.WebService.controllers;
+
+
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.WebService.MimeTypes;
+
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicException;
+import net.sf.jmimemagic.MagicMatchNotFoundException;
+import net.sf.jmimemagic.MagicParseException;
+
+@Controller
+public class AppController {
+
+    @RequestMapping (value = "/", method = RequestMethod.GET)
+    public String get(HttpServletRequest request) {
+        return "fileUpload";
+	}
+
+	/**
+	 * @param file
+	 * @param model
+	 * @return
+	 * @throws MagicParseException
+	 * @throws MagicMatchNotFoundException
+	 * @throws MagicException
+	 * @throws IOException
+	 */
+	@RequestMapping (value = "/uploadFile", method=RequestMethod.POST)
+	public String saveFile(@RequestParam("file") MultipartFile file, Model model)
+		throws MagicParseException, MagicMatchNotFoundException, MagicException, IOException {
+			System.out.println("--------" + file.getOriginalFilename());
+	
+	if (!file.getOriginalFilename().matches("^.*(png)$")){
+			model.addAttribute("fileError", Boolean.TRUE);
+			return "fileUpload";
+	}
+	final String mimeType = Magic.getMagicMatch(file.getBytes()).getMimeType();
+	if (!mimeType.equalsIgnoreCase(MimeTypes.CSV)){
+		model.addAttribute("mimeError", Boolean.TRUE);
+		return "fileUpload";
+	}
+	
+	String baseDir = "F:/Projekt/WebService/src/main/resources/static/upload/";
+	model.addAttribute("success", Boolean.TRUE);
+	file.transferTo(new File(baseDir + file.getOriginalFilename()));
+	return "fileUpload";
+}
+    
+}
